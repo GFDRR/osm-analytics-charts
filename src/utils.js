416b594cca26da73c1d7f793ae568c20de7e7ca3
vectorsize <vectorsize@gmail.com>
2017-06-21 17:44:18 +0200

    small formatting

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export const mountComponent = (Component, selector, options) => ReactDOM.render(React.createElement(Component, options), document.querySelector(selector))
+export const mountComponent = (Component, selector, options) =>
+  ReactDOM.render(React.createElement(Component, options), document.querySelector(selector))
