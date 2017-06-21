ec7503428d5e603ce9d27fedf21f92b27e4312d8
vectorsize <vectorsize@gmail.com>
2017-06-21 15:16:54 +0200

    add scss linter and autofix attempt

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,7 +4,7 @@ import styles from './app.scss'
 class App extends Component {
   render () {
     return (
-      <div className={styles.app}><h1 className='linterror'>hola</h1></div>
+      <div className={styles.app}><h1 className="linterror">hola</h1></div>
     )
   }
 }
