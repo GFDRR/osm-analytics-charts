4dd9fe610c2ac72ec7afc71de312815379c7b4dd
vectorsize <vectorsize@gmail.com>
2017-06-22 09:51:05 +0200

    replace react deps with preact equivalences

--- src/before-after/before-after.jsx
+++ src/before-after/before-after.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import { h } from 'preact'
 import cx from 'classnames'
 
 import { mountComponent } from 'utils'
@@ -7,10 +7,6 @@ import styles from './before-after.scss'
 
 const BeforeAfter = ({ width }) =>
   <div style={{ width }} className={cx(styles.ba, appStyles.viz)}>Before & After</div>
-
-BeforeAfter.propTypes = {
-  width: PropTypes.string
-}
 
 export default function topContributors (selector, options) {
   return mountComponent(BeforeAfter, selector, options)
