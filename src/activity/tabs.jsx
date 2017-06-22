e207d0dcea16924e2da513779de9f60b94c668a7
vectorsize <vectorsize@gmail.com>
2017-06-22 13:09:55 +0200

    add tab component

---
+++ src/activity/tabs.jsx
@@ -0,0 +1,18 @@
+import { h } from 'preact'
+import styles from './activity.scss'
+import cx from 'classnames'
+
+const facets = {
+  f: 'Features',
+  u: 'Users'
+}
+
+const Tabs = ({onClick, facet}) => <div class={styles.tabs}>
+  {Object.keys(facets).map(f => {
+    return <span
+      class={cx(styles.tab, {[styles['tab-selected']]: f === facet})}
+      onClick={() => onClick(f)}>{facets[f]}</span>
+  })}
+</div>
+
+export default Tabs
