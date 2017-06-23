5b8cdebcc52437b220f355b9ddcaae3c2eb5321c
vectorsize <vectorsize@gmail.com>
2017-06-23 09:25:16 +0200

    reformat datastructure into list of lists and make utils generic

--- src/activity/components/histogram/histogram.jsx
+++ src/activity/components/histogram/histogram.jsx
@@ -1,31 +1,27 @@
 import { h } from 'preact'
-import styles from './histogram.scss'
 import cx from 'classnames'
+import * as _ from 'utils'
 
 import Bars from './bars'
+import styles from './histogram.scss'
 
-const key = m => Object.keys(m)[0]
-const values = m => m[key(m)]
-const monthToLabel = m => key(m).substring(0, 3)
-const max = m => Math.max(...values(m))
-const sum = m => values(m).reduce((a, b) => a + b, 0)
-const avg = m => sum(m) / values(m).length
-const avgToColor = (d, m) => `rgba(0, 0, 255, ${avg(m) / 100})`
+const shorten = m => (m).substring(0, 3)
+const avgToColor = (d, m) => `rgba(0, 0, 255, ${_.avg(m) / 100})`
 
-const Histogram = ({ data, margin = 1, className }) => {
+const Histogram = ({ data, margin = 1, monthNames, className }) => {
   return (
     <div class={cx(className, styles.histogram)}>
-      {data.map(month =>
+      {data.map((month, i) =>
         <div
           class={styles['histogram-month']}
           style={{ width: `calc(100% / ${data.length})` }}
         >
-          <Bars max={max(month)} data={values(month)} />
+          <Bars max={_.max(month)} data={month} />
           <div
             style={{ borderColor: avgToColor(data, month) }}
             class={styles['histogram-month-label']}
           >
-            {monthToLabel(month)}
+            {shorten(monthNames[i])}
           </div>
         </div>
       )}
