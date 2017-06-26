1588a97793a5d929c838b53a033d820c5237d769
vectorsize <vectorsize@gmail.com>
2017-06-26 12:56:28 +0200

    replace lodash utils

--- src/utils.js
+++ src/utils.js
@@ -3,10 +3,6 @@ import { h, render } from 'preact'
 export const mountComponent = (Component, selector, options) =>
   render(h(Component, options), document.querySelector(selector))
 
-export const sum = a => a.reduce((a, b) => a + b, 0)
-export const avg = a => sum(a) / a.length
-export const max = m => Math.max(...m)
-
 export const chunk = (arr, chunkSize) => {
   var R = []
   for (var i = 0, len = arr.length; i < len; i += chunkSize) {
