c42b4247ecea29338c572c668615d4c3403b44a5
vectorsize <vectorsize@gmail.com>
2017-06-30 14:37:04 +0200

    country should be added in wp-land

--- public/env.sample.js
+++ public/env.sample.js
@@ -1,5 +1,5 @@
 window.process = {
   env: {
-    SANDBOX_ENDPOINT: 'http://54.224.10.82/api/v1/stats/buildings/country/HTI'
+    SANDBOX_ENDPOINT: 'http://54.224.10.82/api/v1/stats/all/country'
   }
 }
