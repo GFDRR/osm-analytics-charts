a3635e6114159b2cfed81c79e981aefcf47bc67b
vectorsize <vectorsize@gmail.com>
2017-08-10 18:48:00 +0200

    fixes labels from-to and cleans years in between

--- src/activity/components/histogram/labels.jsx
+++ src/activity/components/histogram/labels.jsx
@@ -6,31 +6,42 @@ import styles from './histogram.scss'
 
 const monthName = i => MONTH_NAMES[i]
 const shortenMonth = m => m.substr(0, 3)
-const shortenyear = y => y.substr(2)
 const shortMonthName = i => shortenMonth(monthName(i))
 
 const Labels = ({ index, firstItemIndex, monthIndex, year, numMonths }) => {
+  const labelLength = 6
   const isJanuary = monthIndex === 0
   const isFirst = index === 0
-  const isLate = index > numMonths - 3
+  const isLast = index === numMonths - 1
+  const overlapsLast = index > numMonths - labelLength * 2
+  const overlapsFirst = isJanuary && firstItemIndex - index > 0
 
   const labelClasses = cx({
-    [styles.label]: isJanuary || numMonths <= 1 || isFirst,
+    [styles.label]: isJanuary || numMonths <= 1 || isFirst || isLast,
     [styles.labelHidden]:
-    (numMonths >= 24 && !isJanuary && (!isFirst || firstItemIndex > 7)) ||
-      (numMonths >= 24 && isLate),
-    [styles.labelBorder]: numMonths > 24 && (isJanuary || isFirst)
+    (numMonths >= 24 && !isFirst && !isLast && !isJanuary) ||
+      (numMonths >= 24 &&
+        ((!isFirst && overlapsFirst) || (!isLast && overlapsLast))),
+    [styles.labelBorder]: numMonths > 24 && ((isJanuary && !isLast) || isFirst),
+    [styles.labelBorderLast]: numMonths > 24 && isLast,
+    [styles.last]: isLast
   })
 
   const yearClasses = cx(styles.year, {
-    [styles.yearVisible]: isFirst || isJanuary || numMonths <= 1
+    [styles.yearVisible]: isFirst || isLast || isJanuary || numMonths <= 1
+  })
+
+  const monthClasses = cx({
+    [styles.monthHidden]: numMonths >= 24 && !isFirst && !isLast
   })
 
   return (
     <span className={labelClasses}>
-      {numMonths <= 1 ? monthName(monthIndex) : shortMonthName(monthIndex)}
+      <span className={monthClasses}>
+        {numMonths <= 1 ? monthName(monthIndex) : shortMonthName(monthIndex)}
+      </span>
       <span className={yearClasses}>
-        {numMonths <= 1 ? year : shortenyear(year)}
+        {year}
       </span>
     </span>
   )
